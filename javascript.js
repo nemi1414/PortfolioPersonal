cd3701d273c2abe3494df64cb87cc643771e4729
nemi1414 <juanignacionemi@gmail.com>
2025-11-12 13:06:37 -0300

    Agregué pequeños detalles

--- javascript.js
+++ javascript.js
@@ -8,3 +8,16 @@ emailjs.init('UYIUMI6rR0fdRlFW6');
       .then(() => alert('Mensaje enviado!'))
       .catch(err => alert('Error al enviar: ' + JSON.stringify(err)));
   });
+
+  // Navbar fijo al hacer scroll
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+    const headerHeight = header.offsetHeight;
+    
+    if (window.scrollY > headerHeight - nav.offsetHeight) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    }
+});
